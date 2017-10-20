45fc94f5168f5552f0f81609a21a16214ea82ad3
bruno78 <devbruno78@gmail.com>
2017-10-20 14:36:02 -0400

    first commit

---
+++ src/app/portfolio-content/portfolio-content.component.spec.ts
@@ -0,0 +1,26 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PortfolioContentComponent } from './portfolio-content.component';
+
+describe('PortfolioContentComponent', () => {
+    let component: PortfolioContentComponent;
+    let fixture: ComponentFixture<PortfolioContentComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ PortfolioContentComponent ]
+        })
+        .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(PortfolioContentComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should be created', () => {
+        expect(component).toBeTruthy();
+    });
+});
+    
---
+++ src/app/portfolio-content/portfolio-content.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'portfolio-content',
+    templateUrl: './portfolio-content.component.html',
+    styleUrls: ['./portfolio-content.component.css']
+})
+export class PortfolioContentComponent {
+    constructor () {}
+}
+    
